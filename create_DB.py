5a06a79cdae198d5151440f1fe79c32c630d286e
ShutUp-Pls <marcodelgado.1008@gmail.com>
2022-06-20 03:45:15 -0400

    Modificaciones 3FN

--- create_DB.py
+++ create_DB.py
@@ -1,4 +1,3 @@
-# importing required libraries
 import mysql.connector
  
 dataBase = mysql.connector.connect(
@@ -8,46 +7,68 @@ dataBase = mysql.connector.connect(
   database = "Atacama"
 )
  
-# preparing a cursor object
+# Definir objeto cursos
 cursorObject = dataBase.cursor()
 
-# creating table 
-Noticia = """CREATE TABLE IF NOT EXISTS NOTICIA (
-                   Titulo  text NOT NULL,
-                   Fecha Date,
-                   URLs VARCHAR(512),
-                   Texto text NOT NULL
-                   )"""
+# Tablas Simples
+Dueno = """CREATE TABLE IF NOT EXISTS DUENO(
+        ID_DUENO_PK INT PRIMARY KEY,
+        NOMBRE VARCHAR(50),
+        TIPO VARCHAR(10)
+        )"""
+
 Medio = """CREATE TABLE IF NOT EXISTS MEDIO(
-                    Nombres VARCHAR(50),
-                    URLs VARCHAR(512),
-                    Tipo VARCHAR(10),
-                    Region INT,
-                    Pais VARCHAR(20),
-                    Fecha Date 
-                    )"""         
-Duenho = """CREATE TABLE IF NOT EXISTS DUENHO(
-                    Nombre VARCHAR(50),
-                    Tipo VARCHAR(10),
-                    Fecha Date
-                     )"""          
-Mencion = """CREATE TABLE IF NOT EXISTS MENCION(
-                    Nombre  VARCHAR(50),
-                    Wiki VARCHAR(512),
-                    Profesion VARCHAR(30),
-                    Fecha Date,
-                    Nacionalidad VARCHAR(20)    
-                    )"""
-Popularidad = """CREATE TABLE IF NOT EXISTS POPULARIDAD(
-                    Fecha date,
-                    Valor INT
-                    )"""
+        URL_MEDIO_PK VARCHAR(512) PRIMARY KEY,
+        NOMBRE VARCHAR(50),
+        PAIS VARCHAR(20),
+        IDIOMA VARCHAR(20),
+        REGION TINYINT,
+        FECHA_CRE DATE
+        )"""
+
+Persona = """CREATE TABLE IF NOT EXISTS PERSONA(
+        ID_PERSONA_PK INT PRIMARY KEY,
+        POPULARIDAD INT,
+        WIKI VARCHAR(512),
+        NOMBRE VARCHAR(50),
+        PROFESION VARCHAR(50),
+        NACIONALIDAD VARCHAR(20),
+        FECHA_NAC DATE
+        )"""
+
+# Tablas Dependientes
+Noticia = """CREATE TABLE IF NOT EXISTS NOTICIA(
+        URL_NOTICIA_PK VARCHAR(512) PRIMARY KEY,
+        TITULO VARCHAR(512),
+        TEXTO TEXT NOT NULL,
+        FECHA_PUB DATE,
+        URL_MEDIO_FK FOREIGN KEY REFERENCES MEDIO (URL_MEDIO_PK)
+        )""" 
+
+# Tablas Intermedias
+Adquiere = """CREATE TABLE IF NOT EXISTS ADQUIERE(
+        ID_DUENO_FK FOREIGN KEY REFERENCES DUENO (ID_DUENO_PK),
+        URL_MEDIO_FK FOREIGN KEY REFERENCES MEDIO (URL_MEDIO_PK),
+        FECHA_ADQ DATE
+        )"""
+
+Menciona = """CREATE TABLE IF NOT EXISTS MENCIONA(
+        ID_PERSONA_FK FOREIGN KEY REFERENCES PERSONA (ID_PERSONA_PK),
+        URL_NOTICIA_FK FOREIGN KEY REFERENCES NOTICIA (URL_NOTICIA_PK),
+        )"""  
+
+#Popularidad = """CREATE TABLE IF NOT EXISTS POPULARIDAD(
+#                    Fecha date,
+#                    Valor INT
+#                    )"""
  
 
-cursorObject.execute(Noticia)
+cursorObject.execute(Dueno)
 cursorObject.execute(Medio)
-cursorObject.execute(Duenho)
-cursorObject.execute(Mencion)
-cursorObject.execute(Popularidad)
+cursorObject.execute(Persona)
+cursorObject.execute(Noticia)
+cursorObject.execute(Adquiere)
+cursorObject.execute(Menciona)
+#cursorObject.execute(Popularidad)
 
 # cursorObject.execute("SHOW DATABASES")
