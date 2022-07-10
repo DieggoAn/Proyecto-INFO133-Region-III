d92b60a3bd4f68126a06c35d230ffbbb06112157
DieggoAn <dieggoan@outlook.com>
2022-07-10 16:31:41 -0400

    request.py feo

--- request.py
+++ request.py
@@ -14,9 +14,31 @@ dataBase = mysql.connector.connect(
 # Definir objeto cursos
 cursorObject = dataBase.cursor(buffered=True)
 
-def noticiasPublicadas():
-      cursorObject.execute("SELECT URL_NOTICIA,URL_MEDIO FROM NOTICIA;")
-      for i in cursorObject:
-            print(i)
+##contador noticias por medio
+##select URL_MEDIO, count(URL_MEDIO) from noticia group by URL_MEDIO; 
 
-noticiasPublicadas()
+##Persona mencionada en una noticia en un dia especifico //Modificar valor de FECHA_PUB
+##SELECT NOMBRE, FECHA_PUB FROM persona p 
+#JOIN menciona m ON p.ID_PERSONA = m.ID_PERSONA 
+#JOIN noticia n ON m.URL_NOTICIA = n.URL_NOTICIA
+#WHERE FECHA_PUB = "2022-07-10";
+
+
+##Evolucion de popularidad de una persona ##
+#SELECT ID_PERSONA, VALOR from tiene where ID_PERSONA="1";
+cursorObject.execute('SELECT ID_PERSONA, VALOR from tiene where ID_PERSONA="1"')
+
+Lista_POP = list(cursorObject.fetchall())
+
+d1 = [item[1] for item in Lista_POP]
+if d1[5] < d1[4]:
+  print ("Comparacion Popularidad mes actual con mes anterior: -", round(100 - (d1[0]/d1[1])*100)," %")
+elif  d1[5]==d1[4]:
+  print ("Comparacion Popularidad mes actual con mes anterior: +0 %")
+else:
+  print ("Comparacion Popularidad mes actual con mes anterior: +",round (((d1[5]/d1[4])*100)-100)," %")
+print(Lista_POP)
+
+
+##5 medios de prensa mas antiguos de una region 
+##SELECT * FROM medio WHERE REGION = "3" ORDER BY FECHA_CRE ASC LIMIT 5;
