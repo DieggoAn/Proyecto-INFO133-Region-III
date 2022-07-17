b70161d9080f052ce05c59b2ba0a90a9d8f7e842
MatsuokiSF <sebamfuentes.c@gmail.com>
2022-07-17 14:43:12 -0400

    initScrapper_atacamaenlinea

--- Scrappers/scrapper_tamarillano.py
+++ Scrappers/scrapper_tamarillano.py
@@ -4,6 +4,12 @@ from AGENT import USER_AGENT
 from requests import session
 ##
 ##
+import dateutil.parser as parser
+##
+
+def formatoDate(dateRaw):
+    date =  parser.parse(dateRaw)
+    return date.isoformat()
 def searchItem():
     global  session
     global  headers
@@ -16,7 +22,6 @@ def searchItem():
     #
     url = 'https://tierramarillano.cl/regionales/'
     r = session.get(url,headers=headers)
-    #
     articles = r.html.find('article')
     ##
     newslist = []
@@ -29,39 +34,26 @@ def searchItem():
             link    = newsitem.absolute_links
             formatLink = ",".join(link)
             fecha = formatLink[27:37]
-            if(c == 1 ):
-                #FALTA EXTRAER LA NOTICIA POR EL SELECTOR QUE CORRESPONDE
-                #noticia = noticiaText2(formatLink,'.entry-content')
-                noticia = "Eurecaaaa"  
+            fecha    = formatoDate(fecha)
+            if(c < 2 ):
+                noticia = noticiaText(formatLink,'.entry-content',0)
             else:                
-                noticia = noticiaText(formatLink)
-            print("Noticia numero ----------------->>>",c,"<<<--------------------------------------")     
-            print(formatLink)
-            print(noticia)
-            #newslist.append(tuple((formatLink,title,fecha)))
+                noticia = noticiaText(formatLink,'.elementor-widget-container',10)
+            
+            newslist.append(tuple((formatLink,title,noticia,fecha)))
             c+=1
         except Exception as e:
             print(f"Error>:",e)
+            c+=1
     return newslist
 ##
 ##
 #Funcion que Rescata toda la noticia y la retorna en formato str
-def noticiaText(direccion):
-    r2 = session.get(direccion,headers=headers)
-    selector = '.elementor-widget-container'
-    ubicacion = r2.html.find(selector)
-    try:
-        segmentNew  =   ubicacion[10].text
-        return segmentNew
-    except Exception as e:
-        print(f"Error>:",e)
-    
-def noticiaText2(direccion,selector):
+def noticiaText(direccion,selector,posicion):
     r2 = session.get(direccion,headers=headers)
     ubicacion = r2.html.find(selector)
     try:
-        segmentNew  =   ubicacion[10].text
-        allNew = '\n'.join(segmentNew)  
+        segmentNew  =   ubicacion[posicion].text
         return segmentNew
     except Exception as e:
         print(f"Error>:",e)
@@ -71,10 +63,10 @@ def printCMD(data):
     c=1
     for i in data:
         print("Noticia numero ----------------->>>",c,"<<<--------------------------------------")     
-        print(i[0],i[1],i[2])
+        print(i[0],i[1],i[2],i[3])
         c+=1
 
 
-searchItem()
-#printCMD(data)
+data = searchItem()
+print(data)
 
