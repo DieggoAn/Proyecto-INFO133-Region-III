b543eb3ff00e14326c54c66a5a9fdcd9443505b8
DieggoAn <dieggoan@outlook.com>
2022-07-19 20:37:47 -0400

    Limpieza

--- scrappers/atacama_quehaydecierto.py
+++ scrappers/atacama_quehaydecierto.py
@@ -1,15 +1,6 @@
 from requests_html import HTMLSession
 import random
 
-def formatWait(xpath_text):
-  texto = xpath_text
-  texto = texto.replace("/","").replace("@","")
-  c = 0
-  while texto[c] != "]":
-    c += 1
-  return texto[0:c+1]
-from requests_html import HTMLSession
-
 URL_MEDIO = "https://www.elquehaydecierto.cl/bien-comun"
 XPATH_URLS = "//div[@class='pensando-txt']//h4//a//@href"
 
@@ -17,72 +8,89 @@ XPATH_DATE = "//div[@class='date-created-node']//span"
 XPATH_TITLE = "//h1[@class='page-header']"
 XPATH_TEXT = "//div[@class='content content-node']//p"
 
-WAIT_URLS = "div[class='pensando-txt']"
-WAIT_NEWS = "div[class='date-created-node']"
+def formatWait(xpath_text):
+  texto = xpath_text
+  texto = texto.replace("/","").replace("@","")
+  c = 0
+  while texto[c] != "]":
+    c += 1
+  return texto[0:c+1]
 
-USER_AGENT = {'user-agent':"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"}
+WAIT_URLS = formatWait(XPATH_URLS)
+WAIT_DATE = formatWait(XPATH_DATE)
+WAIT_TITLE = formatWait(XPATH_TITLE)
+WAIT_TEXT = formatWait(XPATH_TEXT)
 
 INTENTOS = 5
 
-session = HTMLSession()
+USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+headers = {'user-agent':random.choice(USER_AGENT_LIST) }
 
-async def extraer_xpaths(wait,*args):
-  datos = []
-  c = 1
-  while c <= INTENTOS:
-    try:
-      print("Esperando Selector:", wait)
-      await respuesta.html.page.waitForSelector(wait)
-    except Exception as e:
-      print("[Intento: {}] Espera Fallida:".format(c), e)
-      c += 1
-    else:
-      print("Listo...")
-      break
-  for arg in args:
-    raw_xpath = await respuesta.html.page.xpath(arg)
-    for i in range(len(raw_xpath)):
-      text_xpath = await respuesta.html.page.evaluate('(e) => e.textContent', raw_xpath[i])
-      datos.append(text_xpath)
+async def funcionUrls(url_medio):
+  global list_href
+  list_href = []
+  print("Scrapeando \"", url_medio,"\"")
+  await respuesta.html.page.waitForSelector(WAIT_URLS)
+  page_urls = await respuesta.html.page.xpath(XPATH_URLS)
+  for i in range(0,len(page_urls)):
+    list_href.append(await respuesta.html.page.evaluate('(e) => e.textContent', page_urls[i]))
   await respuesta.html.page.close()
-  return datos
+  print(list_href)
 
-def renderizar(sleep_time,pagina):
+async def funcionNoticia(url_noticia):
+  print("Scrapeando \"", url_noticia,"\"")
+  await respuesta.html.page.waitForSelector(WAIT_DATE)
+  page_fecha = await respuesta.html.page.xpath(XPATH_DATE)
+  text_fecha = await respuesta.html.page.evaluate('(e) => e.textContent', page_fecha[0])
+  print(text_fecha)
+  page_title = await respuesta.html.page.xpath(XPATH_TITLE)
+  text_title = await respuesta.html.page.evaluate('(e) => e.textContent', page_title[0])
+  print(text_title)
+  page_text = await respuesta.html.page.xpath(XPATH_TEXT)
+  text_text = await respuesta.html.page.evaluate('(e) => e.textContent', page_text[0])
+  print(text_text)
+  print()
+  await respuesta.html.page.close()
+
+def renderizar_pagina(url_pagina):
   global respuesta
-  respuesta = session.get(pagina,headers=USER_AGENT)
-  c = 1
-  while c <= INTENTOS:
+  respuesta = sesionHTML.get(url_pagina,headers=headers)
+  intentos = INTENTOS
+  while intentos > 0:
     try:
-      print("Renderizando", pagina)
-      respuesta.html.render(sleep=sleep_time,keep_page=True)
+      print("Renderizando:", url_pagina)
+      respuesta.html.render(sleep=1,keep_page=True)
+      print("Renderizado Existoso...")
     except Exception as e:
-      print("[Intento: {}] Error de Renderizado:".format(c), e)
-      c += 1
+      print("Error de Renderizado:", e)
     else:
-      print("Renderizado Exitoso...")
-      return
-  print("Se omite el scraping de", pagina)
+      intentos = 1
+    intentos -= 1
 
-def extraer_urls():
-  renderizar(2,URL_MEDIO)
-  return session.loop.run_until_complete(extraer_xpaths(WAIT_URLS,XPATH_URLS)) 
-
-def scraper():
-  hrefs = extraer_urls()
-  datos_tupla = []
-  for href in hrefs:
-    url = URL_MEDIO[:31] + href
-    renderizar(0,url)
-    ttf = session.loop.run_until_complete(extraer_xpaths(WAIT_NEWS,XPATH_TITLE,XPATH_TEXT,XPATH_DATE))
-    datos_tupla.append(tuple((url, ttf[0], ttf[1], ttf[2])))
-  return datos_tupla
-
-def scrap_text():
-  hrefs = extraer_urls()
-  textos = []
-  for href in hrefs:
-    url = URL_MEDIO[:31] + href
-    renderizar(0,url)
-    ttf = session.loop.run_until_complete(extraer_xpaths(WAIT_NEWS,XPATH_TEXT))
-    textos.append(ttf[0])
-  return textos
+sesionHTML = HTMLSession()
+renderizar_pagina(URL_MEDIO)
+sesionHTML.loop.run_until_complete(funcionUrls(URL_MEDIO))
+for href in list_href:
+  url_noticia = URL_MEDIO[:31] + href
+  renderizar_pagina(url_noticia)
+  sesionHTML.loop.run_until_complete(funcionNoticia(url_noticia))
+sesionHTML.close()
